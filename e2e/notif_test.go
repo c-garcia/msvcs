18035987dc8e474e022fa720f2bacb10cdba7ea7
Cristobal Garcia <cristobal.garcia@obliquo.eu>
2018-03-23 08:42:15 +0100

    Comment e2e test code

--- e2e/notif_test.go
+++ e2e/notif_test.go
@@ -3,9 +3,6 @@
 package e2e_test
 
 import (
-	"bytes"
-	"encoding/json"
-	"net/http"
 	"os"
 	"testing"
 
@@ -51,58 +48,64 @@ func TestNotifyAllUsersByEmail(t *testing.T) {
 	}
 
 	whenINotifyThemByEmailWithTheMessage := func(orig, subject, body string) {
-		type notifRequest struct {
-			From    string `json:"from"`
-			Subject string `json:"subject"`
-			Body    string `json:"body"`
-		}
-		makeNotifRequest := func(from, subject, body string) (*http.Request, error) {
-			reqData := &notifRequest{from, subject, body}
-			reqBody, err := json.Marshal(reqData)
-			if err != nil {
-				return nil, err
+		t.Fatal("Not implemented")
+		/*
+			type notifRequest struct {
+				From    string `json:"from"`
+				Subject string `json:"subject"`
+				Body    string `json:"body"`
 			}
-			req, err := http.NewRequest(
-				http.MethodPost,
-				notifSrvURL+"notif",
-				bytes.NewBuffer(reqBody),
-			)
-			req.Header.Set("Content-Type", "application/json")
-			req.Header.Set("Accept", "application/json")
-			t.Logf("Sending request: %v", req)
-			if err != nil {
-				return nil, err
+			makeNotifRequest := func(from, subject, body string) (*http.Request, error) {
+				reqData := &notifRequest{from, subject, body}
+				reqBody, err := json.Marshal(reqData)
+				if err != nil {
+					return nil, err
+				}
+				req, err := http.NewRequest(
+					http.MethodPost,
+					notifSrvURL+"notif",
+					bytes.NewBuffer(reqBody),
+				)
+				req.Header.Set("Content-Type", "application/json")
+				req.Header.Set("Accept", "application/json")
+				t.Logf("Sending request: %v", req)
+				if err != nil {
+					return nil, err
+				}
+				return req, nil
 			}
-			return req, nil
-		}
-		req, err := makeNotifRequest(TEST_ORIGIN, TEST_SUBJECT, TEST_BODY)
-		if err != nil {
-			t.Fatalf("Unexpected error when building request: %v", err)
-		}
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			t.Fatalf("Unexpected error when raising request: %v", err)
-		}
-		if resp.StatusCode/100 != 200 {
-			t.Fatalf("Bad response from server: %d", resp.StatusCode)
-		}
+			req, err := makeNotifRequest(TEST_ORIGIN, TEST_SUBJECT, TEST_BODY)
+			if err != nil {
+				t.Fatalf("Unexpected error when building request: %v", err)
+			}
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			if err != nil {
+				t.Fatalf("Unexpected error when raising request: %v", err)
+			}
+			if resp.StatusCode/100 != 200 {
+				t.Fatalf("Bad response from server: %d", resp.StatusCode)
+			}
+		*/
 	}
 
 	ourUsersReceiveTheSentEmails := func(subject, body string) {
-		users, err := users.FindAll()
-		if err != nil {
-			t.Fatalf("Could not retrieve user list: %v", err)
-		}
-		for _, u := range users {
-			isThere, err := mailChecker.CheckEmail(u.EMail, TEST_ORIGIN, TEST_SUBJECT, TEST_BODY)
+		t.Fatal("Not implemented")
+		/*
+			users, err := users.FindAll()
 			if err != nil {
-				t.Errorf("Error when checking e-mail presence: %v", err)
+				t.Fatalf("Could not retrieve user list: %v", err)
 			}
-			if !isThere {
-				t.Errorf("E-mail to: %s expected to be found", u.EMail)
+			for _, u := range users {
+				isThere, err := mailChecker.CheckEmail(u.EMail, TEST_ORIGIN, TEST_SUBJECT, TEST_BODY)
+				if err != nil {
+					t.Errorf("Error when checking e-mail presence: %v", err)
+				}
+				if !isThere {
+					t.Errorf("E-mail to: %s expected to be found", u.EMail)
+				}
 			}
-		}
+		*/
 	}
 
 	givenWeHaveSomeUsers()
